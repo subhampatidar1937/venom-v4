110dc8d51fc0f1655f4a3c25881f8608ed7b1f2c
subhampatidar1937 <subhampatidar2552@gmail.com>
2024-09-03 16:31:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7405779581:AAEtVdQsX3zc28X5c2vvUOv62cpyzjZyNZQ")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("drkhan1937")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6879005889")
 
 
 
